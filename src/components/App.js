fd35bcc7afea370de4a3eb751568a83b2d1fd7fa
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-23 22:42:53 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+console.log(notes);
+// function createNote() {
+//   return <Note key={notes.id} title={notes.title} content={notes.content} />;
+// }
 
 //want to import an array
+//grab the properties of each itme in the array.  call each itmem whatever you want.
+//in this instance called it oneEntry.
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((oneEntry) => (
+        <Note
+          key={oneEntry.key}
+          title={oneEntry.title}
+          content={oneEntry.content}
+        />
+      ))}
       <Footer />
     </div>
   );
